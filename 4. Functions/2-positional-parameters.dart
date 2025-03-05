aa1b941f7ad7d5821b629bcaec97718eaf211586
Jamil226 <jamil138.amin@gmail.com>
2025-03-05 13:06:22 +0500

    updated-code

---
+++ 4. Functions/2-positional-parameters.dart
@@ -0,0 +1,60 @@
+
+
+void main() {
+    // sayHello('Jamil');
+    // sayHello();
+    // sayHello('Ahmad');
+    
+    download('myfile.text');
+    download('myfile2.text', true);
+
+
+    printNumbers([1, 2, 3, 4, 5]); // ✅ Works like *args
+    print('\n');
+    printData(["Ahmad", 25, true, 3.14]); // ✅ Works with mixed data types
+
+}
+
+// void sayHello(String name){
+//     print("Hello ${name}");
+// }
+
+
+// void sayHello([String name = '']){
+//     print("Hello ${name}");
+// }
+
+// void sayHello([String name = '']){
+
+//     if(name.isNotEmpty) name = name.padLeft(name.length + 1);
+//     print("Hello${name}");
+
+// }
+
+void download(String file, [bool open = false]){
+    print('Downloading ${file}');
+    if(open){
+        print('Opening ${file}');
+    } else {
+        print('File is not Opening!');
+    }
+}
+
+void printNumbers(List<int> numbers) {
+  for (var num in numbers) {
+    print(num);
+  }
+}
+
+void printData(List<dynamic> args) {
+  for (var arg in args) {
+    print(arg);
+  }
+}
+
+
+
+// 📝 Assignment for You
+// Write a function sumAll() that takes multiple numbers as a list and returns their sum.
+// Write a function printWords() that takes a list of words and prints each word in uppercase.
+// Write a function describeItems() that takes multiple mixed data types (string, int, bool) and prints each item with its type.
--- 4. Functions/3-named-parameters.dart
+++ 4. Functions/3-named-parameters.dart
@@ -1,5 +1,25 @@
+// Named Arguments
+
+void greet(String name) {  
+    print("Hello, $name!");  
+}
+
+void greet1(String? name) { 
+    /* ?? (Null-Coalescing Operator) */
+    print("Hello, ${name ?? 'Guest'}!"); // ✅ Uses 'Guest' if null
+}
 
 void main() {
+    greet("Ali");  // ✅ Works fine  
+    // greet(null); // ❌ Error: null value not allowed
+    greet1(null); // ✅ Works fine 
+    
+    greet1("Ali");  // Output: Hello, Ali!
 
 }
+
+
+
+
+
  
