5c8ba85c8011b88c7cd9a38c8702ab838929d52f
zainrock41 <153456131+zainrock41@users.noreply.github.com>
2025-03-09 22:11:57 +0500

    Update 4-switch-statement.dart

--- 3. Control Flow/4-switch-statement.dart
+++ 3. Control Flow/4-switch-statement.dart
@@ -47,3 +47,57 @@ Write a Dart program that takes a month number (1-12) and prints:
 
 
 }
+
+
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter a month number (1-12): ");
+  int month = int.parse(stdin.readLineSync()!);
+
+  if (month == 12 || month == 1 || month == 2) {
+    print("Winter");
+  } else if (month >= 3 && month <= 5) {
+    print("Spring");
+  } else if (month >= 6 && month <= 8) {
+    print("Summer");
+  } else if (month >= 9 && month <= 11) {
+    print("Autumn");
+  } else {
+    print("Invalid month number! Please enter a number between 1 and 12.");
+  }
+}
+
+
+
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter a month number (1-12): ");
+  int month = int.parse(stdin.readLineSync()!);
+
+  switch (month) {
+    case 12:
+    case 1:
+    case 2:
+      print("Winter");
+      break;
+    case 3:
+    case 4:
+    case 5:
+      print("Spring");
+      break;
+    case 6:
+    case 7:
+    case 8:
+      print("Summer");
+      break;
+    case 9:
+    case 10:
+    case 11:
+      print("Autumn");
+      break;
+    default:
+      print("Invalid month number! Please enter a number between 1 and 12.");
+  }
+}
